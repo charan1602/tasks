a0640737b16717631420c85b24ad55884d69b133
charan1602 <charanreddy1602@gmail.com>
2024-07-13 22:42:12 +0530

    assignmentt

---
+++ operators_datatypes/index.js
@@ -0,0 +1,63 @@
+// Datatypes 
+// String
+let str="charan"
+console.log("string_value:",str)
+console.log("type0f_variable:",typeof str)
+
+// Number
+let num=6
+console.log("number_value:",num)
+console.log("type0f_variable:",typeof num)
+
+// Boolean
+let bool=true
+console.log("boolean_value:",bool)
+console.log("type0f_variable:",typeof bool)
+
+// Undefined
+let undefined
+console.log("undefined_value:",undefined)
+console.log("type0f_variable:",typeof undefined)
+
+// Null
+let x = null
+console.log("a_value:",x)
+console.log("type0f_variable:",typeof x)
+
+
+
+
+// Mathematical operators
+let a=20
+let b=10
+console.log(a+b)   // Addition
+console.log(a-b)   // subtraction
+console.log(a*b)   // multiplication
+console.log(a/b)   // division
+console.log(a%b)   // modulo
+console.log(a++)   // post increment
+console.log(a--)   // post decrement
+console.log(++a)   // pre increment
+console.log(--a)   // pre decrement
+
+
+// Comparition operators
+let var1=6
+let var2="6"
+
+let result1 = var1==var2
+console.log(result1)   // equal to
+let result2 = var1===var2
+console.log(result2)   // strictly equal to
+let result3 = var1!=var2
+console.log(result3)   //not equal to
+let result4 = var1!==var2
+console.log(result4)   // strictly not equal to
+let result5 = var1<var2
+console.log(result5)   //less than
+let result6 = var1>var2
+console.log(result6)   //greater than
+let result7 = var1<=var2
+console.log(result7)   //less than or equal to equal to
+let result8 = var1>=var2
+console.log(result8)   //greater than or equal to
